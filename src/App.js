a90054d91b6ca8a9192a166f81582724014b487f
Oscar Monroy <oscarmonroy657@gmail.com>
2020-07-03 07:41:27 -0600

    Router is ready and the main compones have been created (initial config)

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Projects from './components/projects/Projects';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+        <Route exact path="/projects" component={Projects}/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
